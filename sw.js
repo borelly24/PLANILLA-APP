29f3b7e80b9a4fbaf5a32ea66952004789313c51
borelly24 <trebol2506@gmail.com>
2026-09-09 16:26:56 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open('planilla-app-v1').then((cache) => {
+            return cache.addAll([
+                './index.html',
+                'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
